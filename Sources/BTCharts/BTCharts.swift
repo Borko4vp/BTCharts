7bbcf90a223d0c580f5f6a92d94750dd2c0218de
Borko Tomić <borko.tomic@avawomen.com>
2021-06-22 13:42:48 +0200

    Initial package setup

---
+++ Sources/BTCharts/BTCharts.swift
@@ -0,0 +1,3 @@
+struct BTCharts {
+    var text = "Hello, World!"
+}
---
+++ Tests/BTChartsTests/BTChartsTests.swift
@@ -0,0 +1,11 @@
+    import XCTest
+    @testable import BTCharts
+
+    final class BTChartsTests: XCTestCase {
+        func testExample() {
+            // This is an example of a functional test case.
+            // Use XCTAssert and related functions to verify your tests produce the correct
+            // results.
+            XCTAssertEqual(BTCharts().text, "Hello, World!")
+        }
+    }
